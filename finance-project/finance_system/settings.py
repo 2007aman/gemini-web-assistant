4b44367888c991b1e626a58ba470760fc6ee4884
2007aman <palrajni073@gmail.com>
2026-04-05 19:12:47 +0530

    added the file

--- finance-project/finance_system/settings.py
+++ finance-project/finance_system/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny', # Isse authentication hat jayega
+    ],
+}
